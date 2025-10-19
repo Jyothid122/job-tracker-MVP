d0a84bec34cda07a02f65eae3b0dcd88cf131c5e
jyoo <116643414+Jyothid122@users.noreply.github.com>
2025-10-19 19:50:58 +0400

    path updated

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,16 +1,3 @@
-// api/index.js
 import serverless from "serverless-http";
-import express from "express";
-
-const app = express();
-
-// Optional: redirect root to API
-app.get("/", (req, res) => {
-  res.json({ message: "Backend is running" });
-});
-
-// Import your routes from server.js
-import serverRoutes from "../server.js";
-app.use(serverRoutes);
-
+import app from "../server.js";
 export const handler = serverless(app);
